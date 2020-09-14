18560332607d0229f08ee2838987c9bbc4be2606
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-14 02:47:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,45 +5,41 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-  ]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+# Define won?, full?, draw?, over?, and winner below
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
-  end 
-end 
-    
+  end
+end
 
 def full?(board)
- board.all?{|i| i = "X" || i = "O"}
-end 
+  board.all?{|token| token == "X" || token == "O"}
+end
 
 def draw?(board)
   full?(board) && !won?(board)
-end 
-    
+end
+
 def over?(board)
- if won?(board) || full?(board)
-   return true
- else !won?(board) && !full?(board)
-   return false 
- end 
-end 
+  won?(board) || full?(board)
+end
 
 def winner(board)
-  if winning_board = won?(board)
-    board[winning_board.first]
+  if winning_combo = won?(board)
+    board[winning_combo.first]
   end
-end 
+end
   
     
